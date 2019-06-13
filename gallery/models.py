fb80b78305d8f4c85cd7bbcdd5fb392a5223adfa
josephmuema <jmuema95@gmail.com>
2019-06-13 16:23:58 +0300

    created a class for categories

--- gallery/models.py
+++ gallery/models.py
@@ -11,17 +11,17 @@ class Location(models.Model):
     def save_location(self):
         self.save()
 
-# class Category(models.Model):
-#     cname = models.CharField(max_length=30)
+class Category(models.Model):
+    cname = models.CharField(max_length=30)
 
-#     def __str__(self):
-#         return self.cname
+    def __str__(self):
+        return self.cname
 
-#     class Meta:  # Meta subclass specifies model-specific options. This helps in ordering data
-#         verbose_name_plural = 'Categories'
+    class Meta:  # Meta subclass specifies model-specific options. This helps in ordering data
+        verbose_name_plural = 'Categories'
 
-#     def save_category(self):
-#         self.save()
+    def save_category(self):
+        self.save()
 
 
 # class Image(models.Model):
