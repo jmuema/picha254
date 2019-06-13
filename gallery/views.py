ff4b18ae633b6b1116376e12e9fcca07e82afdfc
josephmuema <jmuema95@gmail.com>
2019-06-13 16:22:58 +0300

    created a function to help with the search results

--- gallery/views.py
+++ gallery/views.py
@@ -7,18 +7,18 @@ def initial(request):
         all_images = Image.objects.all()
         return render(request, 'search.html',{"all_images":all_images, "locations": locations})
 
-# def search_results(request):
-#         locations = Location.objects.all()
-#         if 'searchCategory' in request.GET and request.GET["searchCategory"]:
-#                 search_term = request.GET.get("searchCategory")
-#                 searched_images = Image.search_by_category(search_term)
-#                 message= f"Showing: {search_term} pictures"
+def search_results(request):
+        locations = Location.objects.all()
+        if 'searchCategory' in request.GET and request.GET["searchCategory"]:
+                search_term = request.GET.get("searchCategory")
+                searched_images = Image.search_by_category(search_term)
+                message= f"Showing: {search_term} pictures"
                 
-#                 return render(request, 'search.html' , {"message":message, "all_images": searched_images, "locations": locations})       #images is a key while searched_images is a value
+                return render(request, 'search.html' , {"message":message, "all_images": searched_images, "locations": locations})       #images is a key while searched_images is a value
 
-#         else:
-#                 message = "You haven't searched for anything"
-#                 return render(request, 'search.html' , {"message":message}) 
+        else:
+                message = "You haven't searched for anything"
+                return render(request, 'search.html' , {"message":message}) 
 
 # def specific_location(request, location):
 #         locations = Location.objects.all()
