0687437892e4dc84fb951e19efb1aacc14fd5063
josephmuema <jmuema95@gmail.com>
2019-06-13 20:36:46 +0300

    created a class for images

--- gallery/models.py
+++ gallery/models.py
@@ -24,30 +24,30 @@ class Category(models.Model):
         self.save()
 
 
-# class Image(models.Model):
-#     name = models.CharField(max_length=30)
-#     description = models.CharField(max_length=50)
-#     image_path = models.ImageField(upload_to='photos/')
-#     image_location = models.ForeignKey(Location)
-#     image_category = models.ForeignKey(Category)
+class Image(models.Model):
+    name = models.CharField(max_length=30)
+    description = models.CharField(max_length=50)
+    image_path = models.ImageField(upload_to='photos/')
+    image_location = models.ForeignKey(Location)
+    image_category = models.ForeignKey(Category)
 
-#     def __str__(self):
-#         return self.name
+    def __str__(self):
+        return self.name
 
-#     def save_image(self):
-#         self.save()
+    def save_image(self):
+        self.save()
 
-#     @classmethod
-#     def search_by_category(cls,search_term):         #search for an image using its category.
-#         search_result = cls.objects.filter(image_category__cname__icontains=search_term)
-#         return search_result
+    @classmethod
+    def search_by_category(cls,search_term):         #search for an image using its category.
+        search_result = cls.objects.filter(image_category__cname__icontains=search_term)
+        return search_result
 
-#     @classmethod
-#     def filter_location(cls,location):      #filter images by the location.
-#         filter_imagelocation = cls.objects.filter(image_location__lname__icontains=location)
-#         return filter_imagelocation
+    # @classmethod
+    # def filter_location(cls,location):      #filter images by the location.
+    #     filter_imagelocation = cls.objects.filter(image_location__lname__icontains=location)
+    #     return filter_imagelocation
 
-#     @classmethod
-#     def get_image_by_id(cls,input_id):
-#         retrieved_image = cls.objects.get(id=input_id)
-#         return retrieved_image
+    # @classmethod
+    # def get_image_by_id(cls,input_id):
+    #     retrieved_image = cls.objects.get(id=input_id)
+    #     return retrieved_image
