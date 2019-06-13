55dba53aac706079dacd344e160a0c62e55c8185
josephmuema <jmuema95@gmail.com>
2019-06-13 20:54:29 +0300

    added code for the admin

--- gallery/admin.py
+++ gallery/admin.py
@@ -7,7 +7,7 @@ from .models import Location, Category, Image
 # class ImageAdmin(admin.ModelAdmin):
 #     filter_horizontal=('category',)      #pass in category article field. Value of filter_horizontal must be a list/tuple
 
-# admin.site.register(Image)
-# admin.site.register(Location)
-# admin.site.register(Category)
+admin.site.register(Image)
+admin.site.register(Location)
+admin.site.register(Category)
 
